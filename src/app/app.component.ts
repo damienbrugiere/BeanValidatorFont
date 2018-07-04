e99bbfd98f425475e50d01406cf61de1ed2e2d26
damien <damien.brugiere.btssio@gmail.com>
2018-07-05 01:53:08 +0200

    ajout des fichiers app + navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuList } from './menu-list';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  toggle: boolean;
+  list: Array<MenuList>;
+  constructor(){
+    this.toggle = true;
+    this.list = [
+      new MenuList("Home","fas fa-home","/home"),
+      new MenuList("Bean validations","fas fa-home","/bean-listing"),
+      new MenuList("Projets","fas fa-home","/project"),
+      new MenuList("Classes","fas fa-home","/classes"),
+    ];
+  }
+
+  getStyle() {
+    if(this.toggle) {
+      return "20%";
+    } else {
+      return "";
+    }
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
+import { BeanListingComponent } from './bean-listing/bean-listing.component';
+import { ProjectListingComponent } from './project-listing/project-listing.component';
+import { ClassListingComponent } from './class-listing/class-listing.component';
+import { PropertyListingComponent } from './property-listing/property-listing.component';
+import { HomeComponent } from './home/home.component';
+const appRoutes: Routes = [
+  { path: 'bean-listing', component: BeanListingComponent },
+  {path:'home',component:HomeComponent},
+  {path:'project', component:ProjectListingComponent},
+  {path:'classes', component:ClassListingComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeanListingComponent,
+    ProjectListingComponent,
+    ClassListingComponent,
+    PropertyListingComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
